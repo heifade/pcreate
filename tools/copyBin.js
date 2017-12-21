29d5dc0dc9249de691182f9d29066a0041370021
heifade <heifade@126.com>
2017-12-21 16:16:02 +0800

    edit tools

---
+++ tools/copyBin.js
@@ -0,0 +1,3 @@
+let fs = require("fs-extra");
+
+fs.copySync("./src/bin", "./es/bin");
---
+++ tools/deleteOutPath.js
@@ -0,0 +1,11 @@
+// 删除dist目录
+
+let fs = require("fs-extra");
+
+function deletePath(path) {
+  fs.emptyDirSync(path);
+  fs.rmdirSync(path);
+}
+
+deletePath("./es");
+deletePath("./docs");
