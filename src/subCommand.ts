5fe80540bae8be9bdb6b48b9593ef4ef941bfe1a
heifade <heifade@126.com>
2017-12-23 20:37:42 +0800

    1.0.0-beta20

--- src/subCommand.ts
+++ src/subCommand.ts
@@ -2,7 +2,7 @@ import { command, Argv } from "yargs";
 
 export async function subCommand() {
   return new Promise((resolve, reject) => {
-    let subCmd = command("pc-node", "node", (yargs: Argv) => {
+    let subCmd = command("pc-node", "pc-node", (yargs: Argv) => {
       resolve(yargs);
       return yargs;
     }).argv;
