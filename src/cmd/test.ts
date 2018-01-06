734d2a6cf27fae6634f0627d3e23451359ec60a4
heifade <heifade@126.com>
2018-01-06 17:17:32 +0800

    1.0.0-beta76

--- src/cmd/test.ts
+++ src/cmd/test.ts
@@ -7,7 +7,7 @@ import { NodeBuilder } from "./build/nodeBuilder";
 import { printMessage, printSuccessMessage, printErrorMessage } from "../common/log";
 import { projectConfigFile } from "../common/const";
 import { getCreateProjectDependencies } from "../common/util";
-import { spawn, execFile, execFileSync, exec } from "child_process";
+import { spawnSync, SpawnSyncOptionsWithStringEncoding, execSync } from "child_process";
 
 export let command = "test";
 export let desc = "单元测试";
@@ -30,23 +30,25 @@ export let handler = (yargs: any) => {
   let nyc = getCreateProjectDependencies(projectPath, path.join("nyc", "bin", "nyc.js"));
   let mocha = getCreateProjectDependencies(projectPath, path.join("mocha", "bin", "mocha"));
 
-  let childProcess = spawn(nyc, [mocha, "-t", "5000"], {
+  let options: SpawnSyncOptionsWithStringEncoding = {
+    encoding: "utf8",
     cwd: projectPath,
     stdio: [process.stdin, process.stdout, process.stderr]
-  });
+  };
 
-  childProcess.on("exit", code => {
-    printMessage("单元测试结束");
-    if (code != 0) {
-      process.exit(code);
-    }
-  });
+  let childProcess = spawnSync(nyc, [mocha, "-t", "5000"], options);
 
-  exec(`"${nyc}" report --reporter=text-lcov | coveralls`, { encoding: "utf-8" }, (err, stdout, stderr) => {
-    if (err) {
-      process.exit(1);
-    } else {
-      printMessage("覆盖率完成" + stdout);
-    }
-  });
+  printMessage("单元测试结束");
+  if (childProcess.status !== 0) {
+    process.exit(childProcess.status);
+    return;
+  }
+
+  // let coveralls = getCreateProjectDependencies(projectPath, path.join("coveralls", "bin", "coveralls.js"));
+
+  let commandText = `"${nyc}" report --reporter=text-lcov`;
+
+  let res = execSync(commandText, { encoding: "utf-8", cwd: projectPath });
+
+  printMessage("覆盖率完成");
 };
