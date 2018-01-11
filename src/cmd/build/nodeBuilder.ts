3c48299c4ea7f1f569e8f4de4b2b3cd84c5e9bac
heifade <heifade@126.com>
2018-01-11 12:47:05 +0800

    1.0.0-beta123

--- src/cmd/build/nodeBuilder.ts
+++ src/cmd/build/nodeBuilder.ts
@@ -94,7 +94,7 @@ module.exports = require('../');
 
     //console.log('typedoc', typedoc,  existsSync(typedoc));
 
-    await asyncExec(typedoc, ["--out", docs, src, "--module", "commonjs", "--hideGenerator"]);
+    await asyncExec(typedoc, ["--out", docs, src, "--module", "commonjs", "--hideGenerator", "--lib", "lib.es2015.d.ts"]);
 
     await format(docs);
   }
