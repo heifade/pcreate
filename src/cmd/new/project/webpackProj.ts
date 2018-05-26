ca0d46dc8185f6cd065e2e94444ccd6624604a1d
heifade <heifade@126.com>
2018-05-26 12:23:36 +0800

    1.0.0-beta160

--- src/cmd/new/project/webpackProj.ts
+++ src/cmd/new/project/webpackProj.ts
@@ -20,31 +20,5 @@ export class WebpackProj extends BaseProj {
 
     let templateZipFile = TemplateData.getProjectTemplate("webpack.zip");
     await unzipPath(templateZipFile, GlobalData.projectRootPath);
-
-    this.saveWebpackConfig();
-    this.savePackage();
-  }
-
-  private async saveWebpackConfig() {
-    await editFile(path.join(GlobalData.projectRootPath, "webpack.config.ts"), fileContent => {
-      return fileContent;
-    });
-  }
-
-  private async savePackage() {
-    await editPackageJson(GlobalData.projectRootPath, json => {
-      Object.assign(json.devDependencies, {
-        "@types/webpack": "^3.8.1",
-        webpack: "^3.10.0",
-        "ts-loader": "^3.2.0",
-        "babel-core": "^6.26.0",
-        "babel-loader": "^7.1.2",
-        "babel-preset-es2015": "^6.24.1"
-      });
-
-      Object.assign(json.scripts, {
-        tsBuild: "webpack"
-      });
-    });
   }
 }
